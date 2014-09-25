0a3b541ba20eb9f51c4e578d717b65d9ca19402d
ctdk <jbingham@gmail.com>
2014-09-24 18:36:43 -0700

    fix formatting of doc.go

--- doc.go
+++ doc.go
@@ -42,15 +42,21 @@ though, so for now installing on non-Debian platforms will have to install schob
 by hand. If you already have a binary you can skip to number 2.
 
 0. Set up go and configure go. (http://golang.org/doc/install.html)
+
 1. Download schob and its dependencies.
 	go get -t github.com/ctdk/schob
 2. Install the schob binary.
 	go install github.com/ctdk/schob
 Alternately, if you downloaded a precompiled binary, put that binary somewhere in your PATH.
+
 3. Make sure goiardi is running on its server, along with serf, and that it's configured to use serf and shovey. You will also need to make the RSA public/private key pair for signing and verifying shovey jobs.
+
 4. Start up serf on the node, making sure that it joins the same serf cluster goairdi's serf is running in.
+
 5. Make sure the shovey signing public RSA key is installed on the node.
+
 6. Create a whitelist.json file for the node, with whitelisted jobs that are allowed to run on the node. See the example whitelist.json file in `test/whitelist.json` for guidance.
+
 7. Run schob. Schob can take a configuration file (an example is provided in `test/schob-example.conf`, or it can use the following command line options:
 
   -v, --version          Print version info.
@@ -112,18 +118,27 @@ https://github.com/ctdk/knife-shove, but here's a cheat sheet:
 Contributing
 
 1. Fork the repository on Github
+
 2. Create a named feature branch (like `add_component_x`)
+
 3. Write your change
+
 4. Write tests for your change (if applicable)
+
 5. Run the tests, ensuring they all pass
+
 6. Submit a Pull Request using Github
 
 See Also
 
 * goiardi (https://github.com/ctdk/goiardi)
+
 * knife-shove (https://github.com/ctdk/knife-shove)
+
 * shovey-jobs cookbook (https://github.com/ctdk/shovey-jobs)
+
 * Goiardi's shovey documentation (https://github.com/ctdk/goiardi/blob/serfing/README.md#shovey)
+
 * Shovey API documentation (https://github.com/ctdk/goiardi/blob/serfing/shovey_api.md)
 
 Author
@@ -141,5 +156,4 @@ details.
 
 "Schob" is German for "shoved".
 */
-
 package main
